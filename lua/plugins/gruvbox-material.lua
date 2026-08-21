117ce896e496389e200f279049c455f9ced40574
Vickrant <vickrantselvendran@gmail.com>
2026-08-21 22:22:48 +0530

    renamed theme name to correct name

--- lua/plugins/gruvbox-material.lua
+++ lua/plugins/gruvbox-material.lua
@@ -6,7 +6,6 @@ return {
 		init = function()
 			vim.g.gruvbox_material_background = "hard"
 			vim.g.gruvbox_material_foreground = "mix"
-			vim.g.gruvbox_material_better_performance = 1
 		end,
 	},
 	{
